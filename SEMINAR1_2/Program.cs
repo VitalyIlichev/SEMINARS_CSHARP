14e2cf2ac32f16e06d9969479230725861a240e1
Виталий <mardonzz@yandex.ru>
2024-02-20 23:43:26 +0300

    Add Seminar1_2

---
+++ SEMINAR1_2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное целое число 
+//и на выходе показывает сумму первой и последней цифры этого числа.
+
+// Console.Write("Введите трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num1 = num%10; // 123%10 -> 3; 123%100 -> 23
+// int num2 = num/100;
+// Console.Write($"Сумма {num1} и {num2} равна {num1 + num2}");
+
+Console.Write("Введите трехзначное число: ");
+string num = Console.ReadLine()!;
+Console.WriteLine(Convert.ToInt32(Convert.ToString(num[0])) +
+Convert.ToInt32(Convert.ToString(num[2])));
