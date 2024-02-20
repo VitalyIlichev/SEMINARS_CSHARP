254a693b6ee6e14e4af0e84c9ff2e9439f6d5bef
Виталий <mardonzz@yandex.ru>
2024-02-20 23:08:41 +0300

    Mod Seminar1_1

--- Seminar1_1/Program.cs
+++ Seminar1_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает _x000b_целое число N, а на выходе
+// показывает все целые _x000b_числа в промежутке от -N до N.
+
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+{
+num = num*(- 1);
+}
+int index = - num;
+while (index <= num)
+{
+Console.Write($"{index} ");
+index++; // index++ => index = index + 1
+// index--; => index = index - 1
+// index += 2; => index = index + 2
+}
